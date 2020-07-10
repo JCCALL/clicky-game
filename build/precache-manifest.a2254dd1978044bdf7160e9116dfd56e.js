43deb877df63bee12b0a837478c443014afe8aa2
Dark Knight <jordanccall@gmail.com>
2020-07-10 15:37:31 -0600

    build

---
+++ build/precache-manifest.a2254dd1978044bdf7160e9116dfd56e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7b9afc8e117cb9dd856dd49eb16267",
+    "url": "/JCCALL/clicky-game/index.html"
+  },
+  {
+    "revision": "fc8c43b95ce8b3f01b06",
+    "url": "/JCCALL/clicky-game/static/css/main.5255a3d5.chunk.css"
+  },
+  {
+    "revision": "374cd20b749c6d2b3585",
+    "url": "/JCCALL/clicky-game/static/js/2.5d2e3be2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JCCALL/clicky-game/static/js/2.5d2e3be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc8c43b95ce8b3f01b06",
+    "url": "/JCCALL/clicky-game/static/js/main.2e5ecaea.chunk.js"
+  },
+  {
+    "revision": "2af64590bdd68c7c48e8",
+    "url": "/JCCALL/clicky-game/static/js/runtime-main.b547a879.js"
+  },
+  {
+    "revision": "b05ef3f3a2933de37592a82a76343663",
+    "url": "/JCCALL/clicky-game/static/media/background.b05ef3f3.jpg"
+  },
+  {
+    "revision": "bc9ffb75279920e4012e340bbd48eb20",
+    "url": "/JCCALL/clicky-game/static/media/herculanum.bc9ffb75.ttf"
+  }
+]);
